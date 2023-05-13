fa7ea284bb4aa5214adc6a69bc8b7e330b994fc9
김햇빛 <kmn3626@naver.com>
2023-05-13 02:20:46 +0000

    Committing changes to recipe/views.py

--- recipe/views.py
+++ recipe/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 import openai
 import random
 
-openai.api_key = "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" 
+openai.api_key = "OPENAI_API_KEY"
 
 def recipe_generator(request):
     if request.method == 'POST':        
@@ -11,14 +11,14 @@ def recipe_generator(request):
 
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=f"주어진 재료 : {', '.join(lst)} 어떤 음식을 만들 수 있어? 단계에 따라 알려줘. 그리고, 첫 줄에 음식의 이름을 말해줘.",
+            prompt=f"주어진 재료 : {', '.join(lst)} 어떤 음식을 만들 수 있어? 단계에 따라 알려줘. 첫 줄에 음식의 이름을 말해줘.",
             max_tokens=500,
             temperature=0.6
         )
 
         output = response['choices'][0]['text'].strip('\n')
         output_list = output.split("\n")
-        title = output_list[0]
+        title = output_list[0].replace(':', '').replace('.', '')
         instructions = '\n'.join(output_list[1:])
 
         return render(request, 'recipe.html', {'title': title, 'instructions': instructions, 'ingredients': lst})
